18afe3724fc6d438b7fa952fc1c292e8a4791d7e
Minato <bitcreditscc@gmail.com>
2016-03-23 21:43:52 +0200

    init

---
+++ src/wallet/rpcwallet.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 The Bitcredit Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCREDIT_WALLET_RPCWALLET_H
+#define BITCREDIT_WALLET_RPCWALLET_H
+
+void walletRegisterRPCCommands();
+
+#endif //BITCREDIT_WALLET_RPCWALLET_H
